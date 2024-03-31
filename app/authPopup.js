9b865e656b93e0b64385db555496681c1cee88c1
abhijoshi <joshigolu08@gmail.com>
2024-03-31 23:24:16 +0530

    initial

---
+++ app/authPopup.js
@@ -0,0 +1,91 @@
+// Create the main myMSALObj instance
+// configuration parameters are located at authConfig.js
+const myMSALObj = new msal.PublicClientApplication(msalConfig);
+
+let username = "";
+
+function selectAccount() {
+
+    const currentAccounts = myMSALObj.getAllAccounts();
+    if (currentAccounts.length === 0) {
+        return;
+    } else if (currentAccounts.length > 1) {
+        // Add choose account code here
+        console.warn("Multiple accounts detected.");
+    } else if (currentAccounts.length === 1) {
+        username = currentAccounts[0].username;
+        showWelcomeMessage(username);
+    }
+}
+
+function handleResponse(response) {
+
+    if (response !== null) {
+        username = response.account.username;
+        showWelcomeMessage(username);
+    } else {
+        selectAccount();
+    }
+}
+
+function signIn() {
+
+    myMSALObj.loginPopup(loginRequest)
+        .then(handleResponse)
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function signOut() {
+
+    const logoutRequest = {
+        account: myMSALObj.getAccountByUsername(username),
+        postLogoutRedirectUri: msalConfig.auth.redirectUri,
+        mainWindowRedirectUri: msalConfig.auth.redirectUri
+    };
+
+    myMSALObj.logoutPopup(logoutRequest);
+}
+
+function getTokenPopup(request) {
+
+    request.account = myMSALObj.getAccountByUsername(username);
+    
+    return myMSALObj.acquireTokenSilent(request)
+        .catch(error => {
+            console.warn("silent token acquisition fails. acquiring token using popup");
+            if (error instanceof msal.InteractionRequiredAuthError) {
+                // fallback to interaction when silent call fails
+                return myMSALObj.acquireTokenPopup(request)
+                    .then(tokenResponse => {
+                        console.log(tokenResponse);
+                        return tokenResponse;
+                    }).catch(error => {
+                        console.error(error);
+                    });
+            } else {
+                console.warn(error);   
+            }
+    });
+}
+
+function seeProfile() {
+    getTokenPopup(loginRequest)
+        .then(response => {
+            callMSGraph(graphConfig.graphMeEndpoint, response.accessToken, updateUI);
+        }).catch(error => {
+            console.error(error);
+        });
+}
+
+function readMail() {
+    getTokenPopup(tokenRequest)
+        .then(response => {
+            callMSGraph(graphConfig.graphMailEndpoint, response.accessToken, updateUI);
+        }).catch(error => {
+            console.error(error);
+        });
+}
+
+selectAccount();
